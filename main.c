b2409a03ed610a4428c46d3baa9e787e5c830c92
josepvazquezp <jose.vazquez@iteso.mx>
2021-12-02 23:02:34 -0600

    prueba

--- main.c
+++ main.c
@@ -11,6 +11,9 @@ int main(void)
     const int screenHeight = 900;
     int start = 1;
     int shuffleR = 1;
+    int r2 = 0;
+    int nextround = 0;
+    int winner = 0;
 
     Stack *V1;
     Stack *V2;
@@ -47,6 +50,10 @@ int main(void)
         if(GetKeyPressed() == KEY_ENTER)
             start = 0;
 
+        if (GetKeyPressed() == KEY_N)
+            r2 = 1;
+
+
         if(start == 0)
         {
             //displayPDeck(D, P1, peek(D));
@@ -54,18 +61,31 @@ int main(void)
             if(peek(D) != NULL)
             {
                 itsGoTimeBBY(D, V1, V2, P1, P2, P1R2, P2R2);
+                nextround = 1;
             }
-            else
-                ClearBackground(BLACK);
+            else if(nextround == 1)
+            {
+                drawRound2();
+                r2++;
+            }
 
-            /*
-            while (peek(P1R2) != NULL && peek(P2R2) != NULL)
+            if (peek(P1R2) != NULL && peek(P2R2) != NULL && r2 >= 50 )
             {
                 round2(D, V1, V2, P1R2, P2R2);
+                nextround = 0;
+                winner = 1;
             }
-             */
 
-            //claimWinner(V1, V2);
+            //else
+            //{   if (winner == 1)
+            //{
+            //    claimWinner(V1, V2);
+            //  }
+
+            //}
+
+
+
         }
 
         EndDrawing();
